5468b3d50dc8a7d60281c818350497362bd6751f
jo <95hyouka@gmail.com>
2022-08-08 00:27:59 +0900

    add shell.nix for rust

---
+++ rust/shell.nix
@@ -0,0 +1,13 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [
+      (nixpkgs.rustChannels.stable.rust.override {
+        extensions = ["rust-src" "rust-analysis" ];
+      })
+    ];
+  }
